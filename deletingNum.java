49909bbc0163dda7bd6489fb59387fa71b4295d1
dineshvas <dineshkumar9569@gmail.com>
2017-06-28 14:43:26 +0530

    Add files via upload

---
+++ deletingNum.java
@@ -0,0 +1,28 @@
+package guvi;
+import java.util.Arrays;
+import java.util.Scanner;
+public class deletingNum {
+	public static void main(String[] args) {
+			System.out.println("enter the values");      
+			Scanner sc=new Scanner(System.in);
+			int num=sc.nextInt();
+			String s="";
+			s+=num;
+			//System.out.println(s);
+			char[]c=s.toCharArray();
+			Arrays.sort(c);
+			System.out.println(c);
+			System.out.println("enter the k");
+			Scanner sc1=new Scanner(System.in);
+			int k=sc1.nextInt();
+			for(int i=0;i<k-1;i++)
+		{
+				System.out.print(""+c[i]);
+		}
+			
+			
+	
+		}
+		
+
+}
